9328eeb5a3e4df58d362d72f122bc13223a543b8
jpabloallin <jpac.647@gmail.com>
2022-04-23 01:13:59 -0500

    Score.java Class ---> Method oderListPlayer() and addPlayer()

--- src/com/sofkau/models/Score.java
+++ src/com/sofkau/models/Score.java
@@ -2,7 +2,23 @@ package com.sofkau.models;
 
 import com.sofkau.interfaces.IUtilities;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Score implements IUtilities {
+
+    private ArrayList<Player> listPlayers = new ArrayList<>();
+
+    public ArrayList orderListPlayers() {
+        ArrayList<Player> playersDisplayedOrderely = new ArrayList<>();
+
+        return playersDisplayedOrderely;
+    }
+
+    public void addPlayer(Player player) {
+        this.listPlayers.add(player);
+    }
+
 
     @Override
     public void showMenu() {
