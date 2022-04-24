702006b708aced93d597c52037efdf433cf85c33
jpabloallin <jpac.647@gmail.com>
2022-04-24 00:35:22 -0500

    Game class ----> Methods showMenu and requestOption

--- src/com/sofkau/models/Game.java
+++ src/com/sofkau/models/Game.java
@@ -2,13 +2,43 @@ package com.sofkau.models;
 
 import com.sofkau.interfaces.IUtilities;
 
-public class Game implements IUtilities  {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Game implements IUtilities {
     @Override
     public void showMenu() {
-
+        System.out.println("\n*********BIENVENIDOS AL JUEGO*********\n");
+        System.out.println("1. Iniciar nueva partida");
+        System.out.println("2. Volver al menu principal");
+        System.out.println("3. Salir");
     }
+
     @Override
     public void requestOption() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Ingresa una opción: ");
+        int opcion = 0;
+        try {
+            opcion = keyboard.nextInt();
 
+        } catch (InputMismatchException e) {
+            System.out.print("POR FAVOR INGRESA UN NUMERO ENTRE 1 Y 3 ");
+            showMenu();
+            requestOption();
+        }
+        switch (opcion) {
+            case 1:
+                //nueva partida
+                break;
+            case 2:
+                //menu principal
+                break;
+            case 3:
+                //salir
+                break;
+            default:
+                System.out.print("POR FAVOR INGRESA UN NUMERO ENTRE 1 Y 3, GRACIAS");
+        }
     }
 }
